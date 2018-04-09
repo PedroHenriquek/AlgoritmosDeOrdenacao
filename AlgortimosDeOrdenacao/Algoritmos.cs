2ec4923b144f26b0dee8451c5339167490c9a331
Pedro Henrique <pedrohenrique096k@gmail.com>
2018-04-08 23:48:48 -0300

    Projeto final

---
+++ AlgortimosDeOrdenacao/Algoritmos.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace AlgoritmoDeOrdenacao {
+    class Algoritmos {
+        /* O método lerArquivos utiliza a classe StreamReader para fazer a leitura de um arquivo externo
+        * Após receber esse arquivo realiza uma conversão dentro de uma variavel simples do tipo string
+        * para em seguida colocar em um vetor de string o conteudo do arquivo
+        *
+        * Todo o conteudo do vetor de string é passado para um vetor de inteiro dentro de um laço for
+        * e retorna esse vetor para quem fez o chamado     
+        */
+        public static int[] lerArquivo() {
+            StreamReader file = new StreamReader(@"C:\Users\Pedro Henrique\Desktop\Faculdade\testeDeLeitura.txt");
+            string txt = file.ReadToEnd();
+            string[] separadores = { " ", "\n" };
+            string[] vetS = txt.Split(separadores, StringSplitOptions.RemoveEmptyEntries);
+            int[] guardado = new int[vetS.Length];
+            for (int i = 0; i < vetS.Length; i++) {
+                guardado[i] = int.Parse(vetS[i]);
+            }
+            file.Close();
+            return guardado;
+        }
+
+        /*
+        * Metodo para salvar arquivo depois de ordenar o Vetor
+        * O metodo vai receber um vetor de inteiros já ordenado e salvar cada numero com um espaço em branco entre eles 
+        */
+        public static void salvarArquivo(int[] vetor) {
+            StreamWriter sw = File.CreateText(@"C:\Users\Pedro Henrique\Desktop\Faculdade\gravarNumero.txt");
+            for (int i = 0; i < vetor.Length;i++) {
+                sw.Write(vetor[i] + " ");
+                
+            }
+            sw.Close();
+        }
+
+        public static void insertionSort(int[] vetor) {
+            int i, j, chave;
+            for (i = 1; i < vetor.Length; i++) {
+                chave = vetor[i];
+                j = i;
+                while ((j > 0) && (vetor[j-1] > chave)) {
+                    vetor[j] = vetor[j - 1];
+                    j--;
+                }
+                vetor[j] = chave;
+            }
+        }
+
+        public static void SelectionSort(int[] vetor) {
+            int min, aux;
+            for (int i = 0; i < vetor.Length; i++) {
+                min = i;
+                for (int j = i + 1; j < vetor.Length; j++) {
+                    if (vetor[j] < vetor[min]) {
+                        min = j;
+                    }
+                }
+
+                if (min != i) {
+                    aux = vetor[min];
+                    vetor[min] = vetor[i];
+                    vetor[i] = aux;
+                }
+            }
+        }
+
+        public static void mergeSort(int[] vetor, int[] w, int ini, int fim) {
+            if (ini < fim) {
+                int meio = (ini + fim) / 2;
+                mergeSort(vetor, w, ini, meio);
+                mergeSort(vetor, w, meio + 1, fim);
+                combinar(vetor, w, ini, meio, fim);
+            }
+        }
+
+        public static void combinar(int[] vetor, int[] w, int ini, int meio, int fim) {
+            for (int k = ini; k <= fim; k++) {
+                w[k] = vetor[k];
+            }
+
+            int i = ini;
+            int j = meio + 1;
+
+            for (int k = ini; k <= fim; k++) {
+                if (i > meio) vetor[k] = w[j++];
+                else if (j > fim) vetor[k] = w[i++];
+                else if (w[i] < w[j]) vetor[k] = w[i++];
+                else vetor[k] = w[j++];
+            }
+        }
+
+        public static void stringFormato(TimeSpan ts) {
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+    }
+}
---
+++ AlgortimosDeOrdenacao/ProgramaPrincipal.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace AlgoritmoDeOrdenacao {
+            /*
+            * Atividade de Analise de Algoritmos
+            * Nome: Pedro Henrique - Felipe Pontes
+            * Insertion-Sort || Merge-Sort || Selection-Sort
+            */
+    class ProgramaPrincipal {
+
+        static void Main(string[] args) {
+            int opcao = 0;
+
+            while (opcao != 4) {
+                Console.WriteLine("1- Insertion-Sort");
+                Console.WriteLine("2- Merge-Sort");
+                Console.WriteLine("3- Selection Sort");
+                Console.WriteLine("4- Sair");
+                opcao = int.Parse(Console.ReadLine());
+
+                //Classe Stopwatch para fazer a contagem do tempo de execução de cada método de ordenação
+                Stopwatch tempo = new Stopwatch();
+                //Estrutura Timespan para conversão de tempo vindo da variavel "tempo" de Stopwatch
+                TimeSpan sp = new TimeSpan();
+                //Chamada da leitura de arquivo na classe "Algoritmos"
+                int[] vetorArquivo = Algoritmos.lerArquivo();
+                switch (opcao) {
+                    // Menu simples de opções entre as 3 opções de ordenação
+                    case 1:
+                        int[] teste = new int[vetorArquivo.Length];
+                        tempo.Start();
+
+                        Algoritmos.insertionSort(vetorArquivo);
+  
+                        tempo.Stop();
+                        sp = tempo.Elapsed;
+                        Algoritmos.stringFormato(sp);
+                        Algoritmos.salvarArquivo(vetorArquivo);
+                        break;
+                    case 2:
+                        tempo.Start();
+                        int[] w = new int[vetorArquivo.Length];
+                        Console.Write("\n");
+
+                        Algoritmos.mergeSort(vetorArquivo, w, 0, vetorArquivo.Length - 1);
+
+                        tempo.Stop();
+                        sp = tempo.Elapsed;
+                        Algoritmos.stringFormato(sp);
+                        Algoritmos.salvarArquivo(vetorArquivo);
+                        break;
+                    case 3:
+                        tempo.Start();
+
+                        Algoritmos.SelectionSort(vetorArquivo);
+
+                        Console.Write("Saida: ");
+
+                        tempo.Stop();
+                        sp = tempo.Elapsed;
+                        Algoritmos.stringFormato(sp);
+                        Algoritmos.salvarArquivo(vetorArquivo);
+                        break;
+                    case 4:
+                        Console.WriteLine("Finalizando. Aperte ENTER para encerrar o programa!");
+                        break;
+                    default:
+                        Console.WriteLine("Opção Invalida!");
+                        break;
+                }
+            } 
+        }
+    }
+}
